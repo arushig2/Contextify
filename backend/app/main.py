645afb24e27dd489077828a0daf5644bd5b8c18d
Arushi Gupta <arushig223@gmail.com>
2026-07-31 23:00:48 +0530

    fix: enable CORS for Chrome extension

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
 import uvicorn
-from .api.router import router 
 import logging
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .api.router import router 
 import app.core.logging
 
 logger = logging.getLogger(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(
     title="Contextify",
